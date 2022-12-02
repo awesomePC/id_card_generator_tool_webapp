0fc57f97ff7c1b0a4ce5c2f4afc85773c59fec50
awesomePC <billycrow268@gmail.com>
2022-12-02 09:25:22 -0600

    added docker-composer.yml

--- _keenthemes/settings.py
+++ _keenthemes/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = '_keenthemes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'OCR_DB', 
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
