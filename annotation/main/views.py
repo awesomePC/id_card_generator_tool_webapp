90415c97b738f1553cb162059d07b3ba2cdc9b1d
awesomePC <billycrow268@gmail.com>
2022-12-03 09:56:24 -0800

    refresh_show start

--- annotation/main/views.py
+++ annotation/main/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from _keenthemes.__init__ import KTLayout
 from _keenthemes.libs.theme import KTTheme
 from tasks.models import Tasks
-from annotation.models import DictionaryHub
+from annotation.models import DictionaryHub, LineAnnotation, WordAnnotation
 from django.shortcuts import redirect
 """
 This file is a view controller for multiple pages as a module.
@@ -13,11 +13,14 @@ Refer to urls.py file for more pages.
 
 class AnnotateMainView(TemplateView):
     template_name = 'pages/annotate/index.html'
-    def dispatch(self, request, *args, **kwargs):
+    ID = 0
+    def dispatch(self, request, id, *args, **kwargs):
         if request.session.get('isAuthenticated',False) is False:
             return redirect('/signin')
         else:
-            return super(AnnotateMainView, self).get(request, *args, **kwargs)
+            self.ID = id
+            return super(AnnotateMainView, self).get(request, id, *args, **kwargs)
+       
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
@@ -29,22 +32,20 @@ class AnnotateMainView(TemplateView):
         KTTheme.addJavascriptFile('/annotate/js/FabricJsHistory.js')
         KTTheme.addJavascriptFile('/annotate/js/script.js')
         KTTheme.addCssFile('/annotate/css/style.css')
-        id=self.request.GET.get('id',None)
-        if id !=None:
+        # task_id from request
+        id = self.ID
+        # id=self.request.GET.get('id',None)
+        if id != 0:
+            context['isAnnoExist'] = True
             rec=Tasks.objects.get(id=id)
-            if rec != None:
-                context['image']=rec.MainImageFile
+            context['annotations'] = list(LineAnnotation.objects.values("line_index","type", 
+                                    "text", "is_fixed_text", "is_render_text","box_coordinates", "key_value", "dict_id"))
+            #  values("id", "name"))
         else:
-            context['showTask']=True
-            userId=self.request.session.get('user',None)['id']
-            context['tasks'] = Tasks.objects.filter(CreateByUserId_id=userId)
-        # dictionarys = DictionaryHub.objects.all()
-        # dics = []
-        # for dic in dictionarys:
-        #     temp = {}
-        #     temp['id'] = dic.id
-        #     temp['name'] = dic.name
-        #     dics.append(temp)
-        # context['dictionarys'] = dics
+            context['isAnnoExist'] = False
+        context['showTask']=True
+        userId=self.request.session.get('user',None)['id']
+        context['tasks'] = Tasks.objects.filter(CreateByUserId_id=userId)
+      
         context['dictionarys'] = list(DictionaryHub.objects.values("id", "name"))
         return context
--- annotation/urls.py
+++ annotation/urls.py
@@ -10,12 +10,12 @@ from annotation import views
 app_name = 'annotate'
 
 urlpatterns = [
-    path('main', AnnotateMainView.as_view(template_name = 'pages/annotate/index.html'), name='annotate-main'),
+    path('main/<int:id>', AnnotateMainView.as_view(template_name = 'pages/annotate/index.html'), name='annotate-main'),
     path('wordlevel', AnnotateWordLevelView.as_view(template_name = 'pages/annotate/wordlevel.html'), name='annotate-wordlevel'),
     path('preview', AnnotatePreviewView.as_view(template_name = 'pages/annotate/preview.html'), name='annotate-preview'),
     path('celery-demo', views.celery_demo, name='annotate-celery'),
-    path('save-lineannotate-data', views.save_lineAnnotateData, name='save-lineannodata'),
-    path('save-wordannotate-data', views.save_wordAnnotateData, name='save-wordannodata'),
+    path('save-lineannotate-data', views.save_lineAnnotateData, name='save-lineannotate-data'),
+    path('save-wordannotate-data', views.save_wordAnnotateData, name='save-wordannotate-data'),
     
     path('genratedatapencard', GenrateDataPenCard.as_view(template_name = 'pages/annotate/genratedatapencard.html'), name='genrate-data-pan-card'),
     path('volumeandcomment', VolumeAndComment.as_view(template_name = 'pages/annotate/volume_and_comment.html'), name='volume-and-comment'),
