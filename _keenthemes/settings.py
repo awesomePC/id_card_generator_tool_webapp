5611d0b8b66edab30195d5cf9dfc5ff3a07a86d0
machinePC <billycrow268@gmail.com>
2022-12-03 23:22:02 +0530

    updated media root and media URL

--- _keenthemes/settings.py
+++ _keenthemes/settings.py
@@ -19,8 +19,8 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'assets')
-MEDIA_URL = '/images/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
