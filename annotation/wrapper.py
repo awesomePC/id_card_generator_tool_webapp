fc429b2fceea59016450670dc856a44d6bddce8a
machinePC <billycrow268@gmail.com>
2022-12-05 00:40:43 +0530

    updated code to group word boxes by line

--- annotation/wrapper.py
+++ annotation/wrapper.py
@@ -1,6 +1,7 @@
-from annotation.models import LineAnnotation, WordAnnotation
+from annotation.models import LineAnnotation, WordAnnotation, LineAnnotationExtraInfo
 from tools.utilities.box_helper import convert_annotation_boxes_str_2_list
 from tools.utilities.word_grouping_helper import group_word_annotations_by_line
+from nb_utils.dict_manipulation import get_multi_occurrence_key_value
 
 def group_words_by_line_coordinates(task_id):
     line_annotations = LineAnnotation.objects.filter(task_id=task_id).defer(
@@ -23,4 +24,23 @@ def group_words_by_line_coordinates(task_id):
         line_annotations, word_annotations, sort=True
     )
 
-    from IPython import embed;embed()
+    # update table
+    for single_line_annotation in grouped_words_annotation_by_line:
+        grouped_word_annotations = single_line_annotation["grouped_word_annotations"]
+        if grouped_word_annotations:
+            word_ids = get_multi_occurrence_key_value(grouped_word_annotations, key_name="id", log=False)
+
+            # save line info
+            obj_line_extra_info = LineAnnotationExtraInfo.objects.create(line_id=single_line_annotation["id"])
+
+            # words_obj = WordAnnotation.objects.filter(id__in=word_ids)
+
+            # Now saving the ManyToManyField
+            for word_id in word_ids:
+                obj_line_extra_info.grouped_words_annotation_by_line.add(word_id)
+
+    # from IPython import embed;embed()
+    # pass
+
+    ## TODO: Add metadata file generator method
+    return True
